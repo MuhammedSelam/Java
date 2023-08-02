ab1f1e4144efe9e2b3275fbdcd3887999b783217
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-02 23:59:34 +0300

    v1

---
+++ src/D07_ifStatements/C06.java
@@ -0,0 +1,26 @@
+package D07_ifStatements;
+
+import java.util.Scanner;
+
+public class C06 {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir ucgenin 3 kenar uzunlugunu alin,
+        // ucgen eskenar ise “Eskenar ucgen” yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ucgenin kenar uzunluklarini giriniz");
+
+        double kenar1 = scanner.nextDouble();
+        double kenar2 = scanner.nextDouble();
+        double kenar3 = scanner.nextDouble();
+
+
+        if (kenar1 == kenar2 && kenar1 == kenar3 && kenar1 > 0) {
+
+            System.out.println("Eskenar ucgen");
+        }
+    }
+
+}
