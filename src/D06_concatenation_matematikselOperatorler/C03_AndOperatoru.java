35e5e57e389003a683e3706fe766a1c1afa8cd72
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-02 23:17:39 +0300

    v1

---
+++ src/D06_concatenation_matematikselOperatorler/C03_AndOperatoru.java
@@ -0,0 +1,30 @@
+package D06_concatenation_matematikselOperatorler;
+
+public class C03_AndOperatoru {
+
+    public static void main(String[] args) {
+
+        System.out.println( 5 > 3 && 6 > 4); // true and true ==> true
+
+        System.out.println( 5 < 3 && 6 > 4); // false and true ==> false
+
+        System.out.println( 5 > 3 && 6 <= 4); // true and false ==> false
+
+        System.out.println(5 <= 3 && 6 <= 4); // false and false ==> false
+
+
+        // bir sayinin hem 2, hem 3, hem de 5 ile bolunebilmesi lazim
+
+        int sayi = 24;
+
+        System.out.println( sayi % 2 == 0 && sayi % 3 == 0 && sayi % 5 == 0);
+        //                  true          &&  true         && false ==> false
+
+        // sayi 0 veya daha buyuk ve 100 veya daha kucuk olmalidir
+        // sayi 0 ile 100 arasinda olmalidir(0 ve 100 dahil)
+
+        System.out.println( sayi >= 0 && sayi <= 100);
+        //                   true     && true ==> true
+
+    }
+}
