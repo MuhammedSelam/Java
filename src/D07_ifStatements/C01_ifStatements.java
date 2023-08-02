5286407dc9ec9d91ab221f5104ef2d45a82eaca5
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-02 23:40:09 +0300

    v1

---
+++ src/D07_ifStatements/C01_ifStatements.java
@@ -0,0 +1,15 @@
+package D07_ifStatements;
+
+public class C01_ifStatements {
+
+    public static void main(String[] args) {
+
+        int a = 48;
+        int b = 35;
+
+        if (a > b){
+            System.out.println("a b'den buyuk");
+        }
+
+    }
+}
