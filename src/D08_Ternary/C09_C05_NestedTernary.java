2ce26bf3561b8f7f014e141a66c0fa310a3d8116
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-03 03:27:31 +0300

    v1

---
+++ src/D08_Ternary/C09_C05_NestedTernary.java
@@ -0,0 +1,29 @@
+package D08_Ternary;
+
+import java.util.Scanner;
+
+public class C09_C05_NestedTernary {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan bir tamsayi alin.
+        // Sayi pozitifse, cift sayi veya cift sayi degil seceneklerinden uygun olanib yazdirin
+        // Sayi pozitif degilse, 3 basamakli veya 3 basamakli degil seceneklerinden
+        // uygun olani yazdirin
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lutfen bir tamsayi giriniz: ");
+        int sayi = scanner.nextInt();
+
+        System.out.println( sayi > 0
+                ?
+                sayi % 2 == 0 ? "Cift sayi" : "Tek sayi"
+                :
+                sayi < -99 && sayi > -1000 ? "Sayi negatif ve 3 basamaklidir." : "Sayi negatif ve 3 basamakli degildir.");
+
+
+    }
+}
