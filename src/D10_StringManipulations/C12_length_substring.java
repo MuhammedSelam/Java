3678f2128428265093820496679167b0aa8ea0c0
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-05 00:06:34 +0300

    v1

--- src/D10_StringManipulations/C12_length_substring.java
+++ src/D10_StringManipulations/C12_length_substring.java
@@ -8,7 +8,7 @@ public class C12_length_substring {
 
         // Kullanicidan alinan bir String alin,
         // String’in uzunlugu cift sayi ise tam ortasina :) ekleyin,
-        // String’in uzunlugu tek sayi ise ortadaki harfi silin ve yerine :( yazdirin.
+        // String’in uzunlugu tek sayi ise ortadaki harfi silin ve yerine yazdirin.
 
        Scanner scanner = new Scanner(System.in);
 
