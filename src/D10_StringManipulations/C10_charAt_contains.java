220f5694cbcb6c11a20dbe67b3f984c33ad8a4e1
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-05 00:02:35 +0300

    v1

--- src/D10_StringManipulations/C10_charAt_contains.java
+++ src/D10_StringManipulations/C10_charAt_contains.java
@@ -1,46 +1,43 @@
 package D10_StringManipulations;
 
-public class C10_charAt_contains_length {
+public class C10_charAt_contains {
+
     public static void main(String[] args) {
 
-        // Kullanicidan bir sifre isteyip, asagidaki sartlari kontrol edin
-        // ve kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
+        //  Kullanicidan bir sifre isteyip, asagidaki sartlari kontrol edin ve
+        // kullaniciya duzeltmesi gereken tum eksikleri soyleyin,
         // eger tum sartlari saglarsa, "sifre basariyla kaydedildi" yazdirin
         // - ilk harf kucuk harf olmali
         // - son karakter rakam olmali
         // - sifre bosluk icermemeli
         // - uzunlugu en az 10 karakter olmali
 
-        String sifre= "ab 1453f";
+        String sifre="nasilsinKardes1";
+        int bayrak=0;
 
-        int flag=0;
-
-        // - ilk harf kucuk harf olmali
         if (!(sifre.charAt(0)>='a' && sifre.charAt(0)<='z')){
-            System.out.println("Ilk harf kucuk harf olmali");
-            flag++;
+
+            System.out.println("ilk harf kucuk harf olmali");
+            bayrak++;
         }
 
-        // - son karakter rakam olmali
         if (!(sifre.charAt(sifre.length()-1)>='0' && sifre.charAt(sifre.length()-1)<='9')){
             System.out.println("Son karakter rakam olmali");
-            flag++;
+            bayrak++;
         }
 
-        // - sifre bosluk icermemeli
         if (sifre.contains(" ")){
             System.out.println("Sifre bosluk icermemeli");
-            flag++;
+            bayrak++;
         }
 
-        // - uzunlugu en az 10 karakter olmali
         if (!(sifre.length()>=10)){
-            System.out.println("Sifrenin uzunlugu en az 10 karakter olmali");
-            flag++;
+            System.out.println("Sifre en az 10 haneli olmali");
+            bayrak++;
         }
 
-        if (flag==0){
-            System.out.println("sifreniz basariyla kaydedildi");
+        if (bayrak==0){
+            System.out.println("sifreniz basari ile kaydedildi");
         }
 
     }
