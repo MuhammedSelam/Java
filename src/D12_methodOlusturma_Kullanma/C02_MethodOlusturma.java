d525b24dff11f185ec20f5a30392e93270bcb5a1
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-06 23:21:22 +0300

    v1

---
+++ src/D12_methodOlusturma_Kullanma/C02_MethodOlusturma.java
@@ -0,0 +1,37 @@
+package D12_methodOlusturma_Kullanma;
+
+import java.util.Scanner;
+
+public class C02_MethodOlusturma {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan main method icinde  ayri ayri isim ve soyismini alin
+        // Isim ve soyismi ilk harfleri buyuk diger harfler kucuk olacak sekilde duzenleyip,
+        // isim bosluk soyisim seklinde bize donduren bir method olusturun
+        // input : isim : Ali  soyisim :YILMAZ.     output : Ali Yilmaz
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen isminizi giriniz");
+        String isim = scanner.next();
+
+        System.out.println("Lutfen soyisminizi giriniz");
+        String soyisim = scanner.next();
+
+        System.out.println(isimDuzenle(isim, soyisim));
+
+        String duzenliIsim = isimDuzenle(isim, soyisim);
+
+    }
+
+    public static String isimDuzenle(String isim, String soyisim) {
+
+        isim = isim.substring(0, 1).toUpperCase() + isim.substring(1).toLowerCase();
+        soyisim = soyisim.substring(0, 1).toUpperCase() + soyisim.substring(1).toLowerCase();
+
+        String istenenFormattaIsimSoyisim = isim + " " + soyisim;
+
+        return istenenFormattaIsimSoyisim;
+    }
+}
