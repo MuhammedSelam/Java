b8fd2486b631312d3cdcfec51b63a6ae3d494112
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-06 22:58:25 +0300

    v1

--- src/D11_forLoop/C21_NestedForLoop.java
+++ src/D11_forLoop/C21_NestedForLoop.java
@@ -2,30 +2,28 @@ package D11_forLoop;
 
 import java.util.Scanner;
 
-public class C20_NestedForLoop {
+public class C21_NestedForLoop {
 
     public static void main(String[] args) {
 
-        // dikdortgen bicimde, satir ve sutun numaralarini yazdirin
+        // ucgen icin satir ve sutun numaralarini yazdirin
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Lutfen olusturulacak dikdortgenin satir sayisini giriniz");
+        System.out.println("Lutfen ucgenin satir sayisini giriniz");
         int satir = scanner.nextInt();
 
-        System.out.println("Lutfen olusturulacak dikdortgenin sutun sayisini giriniz");
+        System.out.println("Lutfen ucgenin sutun sayisini giriniz");
         int sutun = scanner.nextInt();
 
         for (int i = 1; i <=satir ; i++) { // satir
 
-            for (int j = 1; j <=sutun ; j++) { // sutun
-
+            for (int j = 1; j <=i ; j++) { // sutun
                 System.out.print(i + ","+j + "    ");
             }
             System.out.println("");
         }
 
-
-
     }
+
 }
