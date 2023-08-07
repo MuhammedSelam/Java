e23ab4c136abae92162205ce9d14ff8091878c1e
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-08 00:21:17 +0300

    v1

---
+++ src/D13_whileLoop/C06_WhileLoop.java
@@ -0,0 +1,29 @@
+package D13_whileLoop;
+
+import java.util.Scanner;
+
+public class C06_WhileLoop {
+
+    public static void main(String[] args) {
+
+        // While loop kullanarak kullanicidan alinan sayinin rakamlar toplamini bulun
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen rakamlar toplamini bulmak istediginiz sayiyi yaziniz");
+        int girilenSayi = scanner.nextInt();
+
+        int sayi = girilenSayi;
+        int birlerBasamagi = 0;
+        int rakamlarToplami = 0;
+
+        while (sayi > 0){
+            birlerBasamagi = sayi % 10;
+            rakamlarToplami += birlerBasamagi;
+            sayi /= 10;
+        }
+
+        System.out.println("girilen " + girilenSayi + "'nin rakamlar toplami : " + rakamlarToplami);
+    }
+}
