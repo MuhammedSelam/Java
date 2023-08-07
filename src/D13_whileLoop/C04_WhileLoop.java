418229566484201e087b29ea45f0d6f9030d41e2
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-08 00:16:54 +0300

    v1

--- src/D13_whileLoop/C04_WhileLoop.java
+++ src/D13_whileLoop/C04_WhileLoop.java
@@ -1,25 +1,25 @@
 package D13_whileLoop;
 
-public class C04_whileLoop {
+public class C04_WhileLoop {
 
     public static void main(String[] args) {
 
         // 1'den 100'e kadar olan sayilari (sinirlar dahil)
         // for loop ile hesaplayin
 
-        int toplam=0;
+        int toplam = 0;
         for (int i = 1; i <= 100 ; i++) {
-            toplam+=i;
+            toplam += i;
         }
         System.out.println("for loop ile toplam : " + toplam); // 5050
 
 
         // while loop ile hesaplayin
 
-        toplam =0;
+        toplam = 0;
         int i = 1;
 
-        while (i<=100){
+        while (i <= 100){
             toplam += i ;
             i++;
         }
---
+++ src/D13_whileLoop/C05_WhileLoop.java
@@ -0,0 +1,24 @@
+package D13_whileLoop;
+
+public class C05_WhileLoop {
+
+    public static void main(String[] args) {
+
+        // While loop kullanarak 2 basamakli 7 ile bolunebilen pozitif tamsayilari yazdirin.
+
+        int sayi = 10;
+
+        while (sayi <= 99) {
+
+            if (sayi % 7 == 0) {
+
+                System.out.print(sayi + " ");
+
+            }
+
+            sayi++;
+
+        }
+
+    }
+}
