bf098e6c9c8a3964d4c25c54a22d26ce5dbe5d6b
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-07 23:50:44 +0300

    v1

---
+++ src/D13_whileLoop/C01_WhileLoop.java
@@ -0,0 +1,26 @@
+package D13_whileLoop;
+
+import java.util.Scanner;
+
+public class C01_WhileLoop {
+
+        public static void main(String[] args) {
+
+            // Kullanicidan toplanmak uzere sayilar isteyin
+            // toplam 500 olur veya gecerse toplami yazdirin.
+
+
+            Scanner scanner = new Scanner(System.in);
+
+            int sayi = 0;
+            int toplam = 0;
+
+            while (toplam <= 500) {
+                System.out.println("Lutfen toplamak uzere sayi giriniz");
+                sayi = scanner.nextInt();
+                toplam += sayi;
+            }
+
+            System.out.println("Girilen sayilarin toplami: " + toplam);
+        }
+    }
