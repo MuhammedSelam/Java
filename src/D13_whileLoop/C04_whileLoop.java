a7e364fc567bfd85e58d6ad6f953d1226687c98a
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-08 00:00:05 +0300

    v1

---
+++ src/D13_whileLoop/C04_whileLoop.java
@@ -0,0 +1,27 @@
+package D13_whileLoop;
+
+public class C04_whileLoop {
+
+    public static void main(String[] args) {
+
+        // 1'den 100'e kadar olan sayilari (sinirlar dahil)
+
+        // for loop ile hesaplayin
+
+        int toplam=0;
+        for (int i = 1; i <= 100 ; i++) {
+            toplam+=i;
+        }
+        System.out.println("for loop ile toplam : " + toplam); // 5050
+
+
+        toplam =0;
+        int i = 1;
+
+        while (i<=100){
+            toplam += i ;
+            i++;
+        }
+        System.out.println("while loop ile toplam : " + toplam); // while loop ile toplam : 5050
+    }
+}
