9ccb48bdb0f122f9cbde2d38b8074dbeba395270
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-08 00:56:23 +0300

    v1

---
+++ src/D14_C06_DoWhileLoop/C01_DoWhileLoop.java
@@ -0,0 +1,31 @@
+package D14_C06_DoWhileLoop;
+
+public class C01_DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        do{
+            System.out.print(i + " ");
+            i++;
+        }while (i <= 20);
+
+
+        /*
+        int i = 1;
+        while (i <= 20){
+            System.out.print(i + " ");
+            i++;
+        }
+        */
+
+
+         /*
+        for (int i = 1; i <= 20 ; i++) {
+            System.out.print(i + " ");
+        }
+        */
+
+
+    }
+}
