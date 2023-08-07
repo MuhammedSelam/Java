925404accc5ef0472b69b6071335d1128ac10ca2
MuahmmedSelam <m.selam2022@gmail.com>
2023-08-07 19:36:38 +0300

    v1

---
+++ src/D12_methodOlusturma_Kullanma/C07.java
@@ -0,0 +1,26 @@
+package D12_methodOlusturma_Kullanma;
+
+public class C07 {
+
+    public static void main(String[] args) {
+
+        // verdigimiz bir sayinin faktoryel degerini yazdiran bir method olusturun
+
+       faktoryelDegeri(4); // 4! = 24
+       faktoryelDegeri(3); // 3! = 6
+
+    }
+
+    public static void faktoryelDegeri(int sayi){
+
+        int faktoryel = 1;
+
+        for (int i = sayi; i >= 1 ; i--) {
+
+            faktoryel *= i;
+        }
+
+        System.out.println(sayi + "! = " + faktoryel);
+
+    }
+}
